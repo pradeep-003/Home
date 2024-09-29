f1a6440e43e6d45f8562293c61e267a39fc5f246
Pradeep-003 <pradeep50953@gmail.com>
2024-09-29 12:38:28 +0530

    today

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
